a154db5d1b42290f5bfe3c35fbbef1768d3ff263
Michael Nickey <mnickey@gmail.com>
2014-10-20 22:55:30 -0700

    added oauth suppot for mn-chirpy

---
+++ mn-chirpy.py
@@ -0,0 +1,14 @@
+import authorization
+import json
+import requests
+from urls import *
+
+def main():
+	""" Main function """
+	auth = authorization.authorize()
+	response = requests.get(TIMELINE_URL, auth=auth)
+	print json.dumps(response.json(), indent=4)
+
+if __name__ == '__main__':
+	main()
+	
